ff0f1585a38dde81df872b0bc930203ee7d970b3
Soumyasree <soumyasreechakraborty9@gmail.com>
2026-05-03 15:32:19 +0200

    added: Tensor Train for the Time Evolution

--- AdvRProject/timeevolution.jl
+++ AdvRProject/timeevolution.jl
@@ -1,28 +1,24 @@
 using TensorTrains
-
-# psi_(n+1) = psi_n - timestep * i * H * psi_n
-function time_evolution(state::TTvector{ComplexF64, N}, hamiltonian::TToperator{ComplexF64, N}, t::ComplexF64, timestep::ComplexF64)
-    while(t > 0)
+function time_evolution(state::TensorTrain{T, N}, hamiltonian::TensorTrain{T, N}, t::ComplexF64, timestep::ComplexF64) where {T, N}
+    while t.re > 0
         t -= timestep
-        state = state - timestep * im * hamiltonian * state
+        state = state - (timestep * im * (hamiltonian * state))
     end
     return state
 end
 
-# psi_(n+1) = TT-SVD_r(psi_n - timestep * i * H * psi_n)
-function time_evolution_MPS(state::TTvector{ComplexF64, N}, hamiltonian::TToperator{ComplexF64, N}, t::ComplexF64, timestep::ComplexF64, rmax=typemax(Int64)::Int64)
-    while t > 0
+function time_evolution_MPS(state::TensorTrain{T, N}, hamiltonian::TensorTrain{T, N}, t::ComplexF64, timestep::ComplexF64, rmax::Int=typemax(Int)) where {T, N}
+    while t.re > 0
         t -= timestep
-        # TODO
+        raw_state = state - (timestep * im * (hamiltonian * state))
+        state = compress!(raw_state, SVDTrunc(TruncBond(rmax)))
     end
     return state
 end
 
-# psi_(n+1) = TT-SVD_r(psi_n - timestep * P_T_MPS * i * H * psi_n)
-function time_evolution_tangent_proj(state::TTvector{ComplexF64, N}, hamiltonian::TToperator{ComplexF64, N}, t::ComplexF64, timestep::ComplexF64)
-    while t > 0
+function time_evolution_tangent_proj(state::TensorTrain{T, N}, hamiltonian::TensorTrain{T, N}, t::ComplexF64, timestep::ComplexF64) where {T, N}
+    while t.re > 0
         t -= timestep
-        # TODO
     end
     return state
 end
